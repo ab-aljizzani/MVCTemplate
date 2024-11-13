f259e1940aa081530a3be06df1a39bd6f8a95650
AbdulrahmanAli <ab.aljizzani@outlook.com>
2024-11-13 22:02:43 +0300

    ChangePortalGetAllModel

--- obj/Debug/net8.0/ClinicApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClinicApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58f40d044e45a57d88df82ade0c0eb5f0944a4e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09574047831ade832b82d268367e549159eb5084")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
