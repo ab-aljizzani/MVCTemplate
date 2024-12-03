38594759a6fe979cf981b329925a8c76d260b705
ab-aljizzani <ab.aljizzani@outlook.com>
2024-12-03 13:03:28 +0300

    PackageInitial

--- obj/Debug/net8.0/ClinicApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClinicApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a39af268df8b5afdafc6221b5e3d50ebe61cf891")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c17938e24bc869783535db05985fc7c35a2137df")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
