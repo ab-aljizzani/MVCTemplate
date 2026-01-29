b1728cc1c3caeb1ce10efb863e50d3938959faaf
557657 <557657@RY2600320>
2026-01-29 11:21:28 +0300

    Commit

--- obj/Debug/net8.0/ClinicApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClinicApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d948931e418abd9b62590e5d670a6ec01b58f88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a153f5286cd20dc51685125c55cc4d09f9952d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
